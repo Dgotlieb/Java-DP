d44dd575c02f75941c4159c4f393fbf86cc14be3
dgotlieb <Dgotlieb8@gmail.com>
2020-12-31 19:34:58 +0200

    Delete BuilderTest.java

--- BuilderTest.java
+++
@@ -1,7 +0,0 @@
-public class BuilderTest {
-    public static void main(String[] args) {
-        BankAccount ba = new BankAccount.BankAccountBuilder().setBranch("Ramat-gan").setOwner("john").build();
-        System.out.println(ba.getBranch());
-        System.out.println(ba.getOwner());
-    }
-}
